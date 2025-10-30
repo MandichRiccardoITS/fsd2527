99dc3215e31db4ace91535e0a79e52058895d411
RickyMandich <ricky.mandich@gmail.com>
2025-10-30 12:02:56 +0100

    aggiornamento 2025 10 30 12:02 [vBETA 0.1.4]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.app')
+@section('title', 'calendario')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>calendario</h1>
+            </div>
+        </div>
+    </div>
+@endsection
