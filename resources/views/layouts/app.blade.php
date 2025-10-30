bdca0f9cde46cfceead75f72c3afa9460d5449a6
RickyMandich <ricky.mandich@gmail.com>
2025-10-30 14:36:46 +0100

    aggiornamento 2025 10 30 14:36 [vBETA 0.1.27]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico?v={{ time() }}">
 
     <title>@yield('title', config('app.name', 'SWUDB'))</title>
 
